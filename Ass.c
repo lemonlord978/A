7a8b9c473a490af3b34501099d7ec5ac90c4b576
lemonlord978 <51532911+lemonlord978@users.noreply.github.com>
2022-08-25 20:56:04 +0700

    Update Ass.c

--- Ass.c
+++ Ass.c
@@ -28,16 +28,24 @@ int main(){
 	}
 	switch (n)
 	{
-		case 1:;
-		case 2:;
-		case 3:;
-		case 4:;
-		case 5:;
-		case 6:;
-		case 7:;
-		case 8:;
+		case 1:
+		break;
+		case 2:
+		break;
+		case 3:
+		break;
+		case 4:
+		break;
+		case 5:
+		break;
+		case 6:
+		break;
+		case 7:
+		break;
+		case 8:
+		break;
 		case 9:
-		printf("Quiting...");
+		printf("Quiting...\n");
 		break;
 	}
 	

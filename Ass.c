ce97db44d424c7601f0f5187803476ddf97b2b7f
lemonlord978 <51532911+lemonlord978@users.noreply.github.com>
2022-08-25 21:21:04 +0700

    Update Ass.c

--- Ass.c
+++ Ass.c
@@ -42,8 +42,8 @@ int main(){
 	do {
 	printf("********ProVip9000ProgramDesignedByNghiaHoangPhu*********\n");
 	printf("*\t\t1: Add new element.\t\t\t*\n");
-	printf("*\t\t2: display array.\t\t\t*\n");
-	printf("*\t\t3: searching element in array.\t\t*\n");
+	printf("*\t\t2: Display array.\t\t\t*\n");
+	printf("*\t\t3: Searching element in array.\t\t*\n");
 	printf("*\t\t4: Delete value in array.\t\t*\n");
 	printf("*\t\t5: Delete element at index.\t\t*\n");
 	printf("*\t\t6: Delete some elements in range.\t*\n");
